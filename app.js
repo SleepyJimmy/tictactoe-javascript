a9204a0168e50f00c720bceb43b268eb39138440
SleepyJimmy <lijunfang97@gmail.com>
2024-10-19 19:05:49 +0100

    Refactored code to use function factories

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
 const gameBoardElement = document.getElementById("gameBoard");
 
 
-let gameBoard = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-];
+function gameBoard() {
+    const board = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
 
-function renderBoard() {
-    for (let row = 0; row < 3; row++) {
-        for (let col = 0; col < 3; col++) {
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
-            gameBoardElement.appendChild(cell);
+    const renderBoard = () => {
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.dataset.row = row;
+                cell.dataset.col = col;
+                cell.textContent = board[row][col];
+                gameBoardElement.appendChild(cell);
+            }
         }
+    };
+
+    const addEntry = (row, col, marker) => {
+        if (board[row][col] === "") {
+            board[row][col] = marker;
+        } else {
+            alert("Cell already taken!");
+        }
+        renderBoard();
     }
+    return {board, renderBoard, addEntry};
 }
 
-renderBoard();
+
+function player(name, marker) {
+    return {name, marker};
+}
+
+
+const board = gameBoard();
+board.renderBoard();
